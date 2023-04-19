8b6cb63d0a815c33387808255ec74b1dadd03d58
Maksym-Zhuravel <allwegotizus1@gmail.com>
2023-04-19 22:06:09 +0300

    Update color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+startBtn.addEventListener('click', changingColorHandler);
+stopBtn.addEventListener('click', discardChangingColorHandler);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+
+function changingColorHandler() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function discardChangingColorHandler() {
+    clearInterval(timerId)
+
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
