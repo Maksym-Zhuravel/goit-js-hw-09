9e48b50d6171dc96745662103ddab334fc8d8c22
Maksym-Zhuravel <allwegotizus1@gmail.com>
2023-04-21 13:47:46 +0300

    Update timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,38 +1,35 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
 const refs = {
-    input: document.querySelector('#datetime-picker'),
-    startBtn: document.querySelector('[data-start]'),
-    daysValue: document.querySelector('[data-days]'),
-    hoursValue: document.querySelector('[data-hours]'),
-    minutesValue: document.querySelector('[data-minutes]'),
-    secondsValue: document.querySelector('[data-seconds]'),
-}
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  daysValue: document.querySelector('[data-days]'),
+  hoursValue: document.querySelector('[data-hours]'),
+  minutesValue: document.querySelector('[data-minutes]'),
+  secondsValue: document.querySelector('[data-seconds]'),
+};
 
-
-
-refs.startBtn.addEventListener('click', onStartTimer)
-refs.startBtn.disabled = true
+refs.startBtn.addEventListener('click', onStartTimer);
+refs.startBtn.disabled = true;
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-    onClose(selectedDates) {
-      
-        if (selectedDates[0] < new Date) {
-          Notiflix.Notify.failure(`Please choose a date in the future`);
-          refs.startBtn.disabled = true
-      } else {
-          refs.startBtn.disabled = false;
-      }
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.failure(`Please choose a date in the future`);
+      refs.startBtn.disabled = true;
+    } else {
+      refs.startBtn.disabled = false;
+    }
   },
 };
 
-flatpickr("input#datetime-picker", options);
+flatpickr('input#datetime-picker', options);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -53,30 +50,19 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
+function onStartTimer() {
+  const timerId = setInterval(() => {
+    const differenceInTime = new Date(refs.input.value) - Date.now();
 
-
-function onStartTimer(){
-
-    const timerId = setInterval(() => {
-        const differenceInTime = new Date(refs.input.value) - Date.now();
-        
-        let timer = convertMs(differenceInTime)
-
+    let timer = convertMs(differenceInTime);
 
     refs.daysValue.textContent = addLeadingZero(timer.days);
     refs.hoursValue.textContent = addLeadingZero(timer.hours);
     refs.minutesValue.textContent = addLeadingZero(timer.minutes);
     refs.secondsValue.textContent = addLeadingZero(timer.seconds);
-    }, 1000)
+  }, 1000);
 }
 
 function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
 }
-
-
-
-
-
-
-
